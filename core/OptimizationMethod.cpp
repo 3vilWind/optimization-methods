e33c1f334c862475d19ff1341b79e6e87c1d4126
3vilWind <evilwind@protonmail.com>
2021-03-18 18:31:55 +0300

    drop useless method

--- core/OptimizationMethod.cpp
+++ core/OptimizationMethod.cpp
@@ -1,12 +1,1 @@
 #include "OptimizationMethod.h"
-
-std::vector<Point> OptimizationMethod::evaluateFunction(double left, double right, double step) {
-    std::vector<Point> result;
-
-    double current = left;
-    while (current <= right) {
-        result.emplace_back(current, function(current));
-        current += step;
-    }
-    return result;
-}
--- core/OptimizationMethod.h
+++ core/OptimizationMethod.h
@@ -13,6 +13,4 @@ public:
     explicit OptimizationMethod(std::function<double(double)> function) : function(std::move(function)) {}
 
     virtual OptimizationMethodDetailedResults minimizeDetails(double left, double right, double epsilon) = 0;
-
-    std::vector<Point> evaluateFunction(double left, double right, double step);
 };

f7e301c6b30dc41ad4d3fc8d22a3725e61075629
3vilWind <evilwind@protonmail.com>
2021-05-20 22:04:46 +0300

    fix

--- lab3/core/tests/SymmetricProfileMatrixTests.cpp
+++ lab3/core/tests/SymmetricProfileMatrixTests.cpp
@@ -7,6 +7,7 @@ class SymmetricProfileMatrixTest : public SymmetricProfileMatrix {
     using SymmetricProfileMatrix::SymmetricProfileMatrix;
 
     FRIEND_TEST(SymmetricProfileMatrixTests, create_from_dense_02);
+
     FRIEND_TEST(SymmetricProfileMatrixTests, create_from_dense_03);
 };
 
@@ -43,20 +44,19 @@ TEST(SymmetricProfileMatrixTests, create_from_dense_02) {
 
 
 TEST(SymmetricProfileMatrixTests, create_from_dense_03) {
-    //теперь симметричная
     DenseMatrix denseMatrix({
-                                    {1, 0, 1},
-                                    {0, 5, 0},
+                                    {1,  0, 1},
+                                    {0,  5, 0},
                                     {11, 0, 9}
                             });
     std::vector<double> diagonal({1, 5, 9});
-    std::vector<size_t> index({0, 0, 0, 0});
+    std::vector<size_t> index({0, 0, 0, 2});
 
     SymmetricProfileMatrixTest a(denseMatrix);
     expect_equal_matrices(a, denseMatrix);
 
     expect_equal_vectors(a.diagonal, diagonal);
-//    expect_equal_vectors(a.index, index);
-//    EXPECT_EQ(a.rowLowerProfile.size(), 0);
-//    EXPECT_EQ(a.columnUpperProfile.size(), 0);
+    expect_equal_vectors(a.index, index);
+    EXPECT_EQ(a.rowLowerProfile.size(), 2);
+    EXPECT_EQ(a.columnUpperProfile.size(), 2);
 }

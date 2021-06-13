c4c80fdbf8b7c6d75c19a6dcb66281d063554cee
3vilWind <evilwind@protonmail.com>
2021-06-13 21:22:59 +0300

    oi

--- lab4/core/methods/Utils.cpp
+++ lab4/core/methods/Utils.cpp
@@ -6,5 +6,5 @@ double oneDimensionalMinimize(const ScalarFunction &f,
                               const Vector &d,
                               double epsilon) {
     Brent method;
-    return method.minimize([&d, &x, &f](double lambda) { return f.compute(x + d * lambda); }, 0, 10, epsilon).result;
+    return method.minimize([&d, &x, &f](double lambda) { return f.compute(x + d * lambda); }, 2 * epsilon, 10, epsilon).result;
 }
